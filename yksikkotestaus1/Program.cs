c69dbf8b669024f7d4848bfb25121d7fc7d2d1f6
Niko Hautamäki <hautamanacco@gmail.com>
2021-03-03 19:40:26 +0200

    Add project files.

---
+++ yksikkotestaus1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yksikkotestaus1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Heikko yksikkötestaus ohjelma");
+            Console.WriteLine("blablablabla");
+            Console.ReadKey();
+        }
+    }
+}
